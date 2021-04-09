c4f0a55d74b944a9ef428d3538f4f062c3827041
João Paulo oliveira <joao.oliveira@kumulus.com.br>
2021-04-09 08:35:43 -0300

    First

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace githubactions_brown_bag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
